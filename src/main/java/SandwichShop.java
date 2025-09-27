14bd4b00390534fbf7a976ee2353324af21fc137
lijas <lijason991@gmail.com>
2025-09-27 19:56:42 -0400

    added supersize option + comments

--- src/main/java/SandwichShop.java
+++ src/main/java/SandwichShop.java
@@ -3,8 +3,10 @@ import java.util.Scanner;
 public class SandwichShop {
     public static void main(String[] args) {
 
+        // new scanner system
         Scanner input = new Scanner(System.in);
 
+        // intro to pick sandwich size
         System.out.println(
                 """
                         Welcome to my sandwich shop!
@@ -15,18 +17,42 @@ public class SandwichShop {
 
         int size = input.nextInt();
 
+        // asks if they want sandwich "loaded"
+        System.out.println("Would you like to supersize that? Y/N");
+        String supersizeStr = input.next();
+        char supersizeChar = Character.toLowerCase(supersizeStr.charAt(0));
+
+        // asks for age
         System.out.println("Please enter your age: ");
         int age = input.nextInt();
 
+        // starts with empty variable
         double price = 0;
 
+        // sets price based on size
         if (size == 1) {
             price = 5.45;
         }
         else if (size == 2) {
             price = 8.95;
         }
+        // if they don't input correctly, they get a large
+        else {
+            System.out.println("Guess that's a Large!");
+            price = 8.95;
+        }
 
+        // if they didn't select no, they get a loaded sandwich
+        if (supersizeChar != 'n' && size == 1)
+        {
+            price += 1;
+        }
+        else if (supersizeChar != 'n' && size == 2)
+        {
+            price += 1.75;
+        }
+
+        // applies discount after everything else
         if (age > 65) {
             price *= 0.8;
         }
@@ -34,6 +60,7 @@ public class SandwichShop {
             price *= 0.9;
         }
 
+        // outputs price as a price with 2 decimal places
         System.out.printf("Your total is $%.2f.", price);
 
 
